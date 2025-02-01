c21801fc49ed2f7f6b1a3593b0934eed47591e8e
nicolaij <nicj@tut.by>
2025-02-01 10:35:13 +0300

    Add USB keyboard

--- MeshRC.h
+++ MeshRC.h
@@ -5,24 +5,22 @@
 #ifndef __MESH_RC_H__
 #define __MESH_RC_H__
 
-#ifdef USE_ESP32
-#include <esp_now.h>
-#include <WiFi.h>
-#endif
 #ifdef USE_ESP8266
 #include <espnow.h>
 #include <ESP8266WiFi.h>
+#else
+#include <esp_now.h>
+#include <WiFi.h>
 #endif
 
 namespace MeshRC
 {
 
 	typedef std::function<void(void)> esp_rc_callback_t;
-#ifdef USE_ESP32
-	typedef std::function<void(const uint8_t *, int)> esp_rc_data_callback_t;
-#endif
 #ifdef USE_ESP8266
 	typedef std::function<void(uint8_t *, uint8_t)> esp_rc_data_callback_t;
+#else
+	typedef std::function<void(const unsigned char *, int)> esp_rc_data_callback_t;
 #endif
 
 	struct esp_rc_event_t
@@ -118,7 +116,75 @@ namespace MeshRC
 		return true;
 	}
 
-#ifdef USE_ESP32
+#ifdef USE_ESP8266
+	void setAddr(uint8_t *addr)
+	{
+		if (esp_now_is_peer_exist(addr))
+			esp_now_del_peer(addr);
+		esp_now_add_peer(addr, ESP_NOW_ROLE_COMBO, 1, 0, 0);
+	}
+
+	esp_now_send_cb_t sendHandler = [](uint8_t *addr, uint8_t sendStatus)
+	{
+		if (sendStatus == 0)
+		{
+#ifndef NO_ESPHOME
+			ESP_LOGD("MeshRC", "message succesfully sent");
+#endif
+		}
+		if (sendStatus == 1)
+		{
+#ifndef NO_ESPHOME
+			ESP_LOGW("MeshRC", "message not succesfully sent");
+#endif
+			sent_error++;
+		}
+		sending = false;
+		duration = micros() - sendTime;
+	};
+
+	esp_now_recv_cb_t recvHandler = [](uint8_t *addr, uint8_t *data, uint8_t size)
+	{
+		static uint8_t offset, i;
+		// Only receives from master if set
+		if (addr == NULL || master == NULL || equals(addr, master, 6))
+		{
+			received++;
+			sender = addr;
+			for (i = 0; i < events_num; i++)
+			{
+				offset = events[i].prefix.length();
+				if (equals(data, (uint8_t *)events[i].prefix.c_str(), offset))
+				{
+					if (events[i].callback)
+						events[i].callback();
+					if (events[i].callback2)
+						events[i].callback2(&data[offset], size - offset);
+				}
+			}
+		}
+		else
+		{
+			ignored++;
+		}
+	};
+
+	void begin()
+	{
+		if (esp_now_init() == OK)
+		{
+			esp_now_is_init = true;
+			if (esp_now_is_peer_exist(broadcast))
+			{
+				esp_now_del_peer(broadcast);
+			}
+			esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
+			esp_now_add_peer(broadcast, ESP_NOW_ROLE_COMBO, 1, 0, 0);
+			esp_now_register_send_cb(sendHandler);
+			esp_now_register_recv_cb(recvHandler);
+		}
+	}
+#else
 	void setAddr(uint8_t *addr)
 	{
 		if (esp_now_is_peer_exist(addr))
@@ -190,75 +256,6 @@ namespace MeshRC
 			peerInfo.encrypt = false;
 			memcpy(peerInfo.peer_addr, broadcast, 6);
 			esp_now_add_peer(&peerInfo);
-			esp_now_register_send_cb(sendHandler);
-			esp_now_register_recv_cb(recvHandler);
-		}
-	}
-#endif
-#ifdef USE_ESP8266
-	void setAddr(uint8_t *addr)
-	{
-		if (esp_now_is_peer_exist(addr))
-			esp_now_del_peer(addr);
-		esp_now_add_peer(addr, ESP_NOW_ROLE_COMBO, 1, 0, 0);
-	}
-
-	esp_now_send_cb_t sendHandler = [](uint8_t *addr, uint8_t sendStatus)
-	{
-		if (sendStatus == 0)
-		{
-#ifndef NO_ESPHOME
-			ESP_LOGD("MeshRC", "message succesfully sent");
-#endif
-		}
-		if (sendStatus == 1)
-		{
-#ifndef NO_ESPHOME
-			ESP_LOGW("MeshRC", "message not succesfully sent");
-#endif
-			sent_error++;
-		}
-		sending = false;
-		duration = micros() - sendTime;
-	};
-
-	esp_now_recv_cb_t recvHandler = [](uint8_t *addr, uint8_t *data, uint8_t size)
-	{
-		static uint8_t offset, i;
-		// Only receives from master if set
-		if (addr == NULL || master == NULL || equals(addr, master, 6))
-		{
-			received++;
-			sender = addr;
-			for (i = 0; i < events_num; i++)
-			{
-				offset = events[i].prefix.length();
-				if (equals(data, (uint8_t *)events[i].prefix.c_str(), offset))
-				{
-					if (events[i].callback)
-						events[i].callback();
-					if (events[i].callback2)
-						events[i].callback2(&data[offset], size - offset);
-				}
-			}
-		}
-		else
-		{
-			ignored++;
-		}
-	};
-
-	void begin()
-	{
-		if (esp_now_init() == OK)
-		{
-			esp_now_is_init = true;
-			if (esp_now_is_peer_exist(broadcast))
-			{
-				esp_now_del_peer(broadcast);
-			}
-			esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
-			esp_now_add_peer(broadcast, ESP_NOW_ROLE_COMBO, 1, 0, 0);
 			esp_now_register_send_cb(sendHandler);
 			esp_now_register_recv_cb(recvHandler);
 		}
---
+++ USBKeyboard.h
@@ -0,0 +1,45 @@
+//
+// Author: Phong Vu
+// Modified by Tom Rust for use with ESPhome
+//
+#ifndef __USBKEYBOARD_H__
+#define __USBKEYBOARD_H__
+
+#include "USB.h"
+#include "USBHIDMouse.h"
+#include "USBHIDKeyboard.h"
+
+namespace USBKeyboard
+{
+
+	USBHIDMouse Mouse;
+	USBHIDKeyboard Keyboard;
+
+	void begin()
+	{
+		Mouse.begin();
+		Keyboard.begin();
+		USB.begin();
+	}
+
+	void write(uint8_t c)
+	{
+		Keyboard.write(c);
+	}
+
+	void writeCTRL(uint8_t c)
+	{
+		Keyboard.press(KEY_LEFT_CTRL);
+		Keyboard.write(c);
+		Keyboard.releaseAll();
+	}
+	void test()
+	{
+		Keyboard.press(KEY_LEFT_GUI);
+		Mouse.move(-100, -100);
+		Mouse.click(MOUSE_LEFT);
+		Keyboard.write('m');
+	}
+} // namespace
+
+#endif
